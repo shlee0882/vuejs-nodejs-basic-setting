d904e89fd6b73b9721014975af213d7bc41f715d
shlee0882 <shlee0882@gmail.com>
2020-10-01 12:03:12 +0900

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1>
+    <h1>{{ age }}</h1>
+    <h1>{{ livingArea }}</h1>
+    <button v-on:click="changeAddress">자식에서 부모로 주소변경</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    name: String,
+    age: Number,
+    livingArea: String,
+  },
+  methods: {
+    changeAddress() {
+      const newAddress = '강원도 속초시';
+      this.$emit('change-address', newAddress);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <hello-world v-bind:name="name"
+      :age="age"
+      :livingArea="livingArea"
+      @change-address="childToParent"
+    >
+    </hello-world>
+    <div><button v-on:click="parentToChild">부모에서 자식으로 주소변경</button></div>
+    <div><button @click="setInitData">원래 주소로 변경</button></div>
+    <div>{{computedName}}</div>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    'hello-world': HelloWorld,
+  },
+  data() {
+    return {
+      name: '초기 데이터',
+      age: 999,
+      livingArea: '초기 주소',
+    };
+  },
+  computed: {
+    computedName() {
+      return this.name.substring(1, 3);
+    },
+  },
+  created() {
+    this.setInitData();
+  },
+  methods: {
+    setInitData() {
+      this.name = '조현영';
+      this.age = 29;
+      this.livingArea = '서울시 송파구';
+    },
+    parentToChild() {
+      this.livingArea = '인천광역시 부평구';
+    },
+    childToParent(val) {
+      this.livingArea = val;
+    },
+  },
+};
+
+</script>
